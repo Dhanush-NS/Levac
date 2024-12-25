a53c829c3b4fbbde8e60255f4ab9c84922492912
Dhanush N S <nsdhanush74@gmail.com>
2024-12-25 19:34:59 +0530

    database updated

--- finalproject/project/project/settings.py
+++ finalproject/project/project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Logincredentials',
+        'NAME': 'Logincred',
         'USERNAME':"root",
         "PASSWORD":"root",
         "HOST":"localhost",
